5aa4d3d8832a8117295c5a42942b27195f526e95
Kelly Hodis <kelly.hodis@processmaker.com>
2020-06-11 15:59:03 -0700

    Add wait to class setup

--- includes/test_parent.py
+++ includes/test_parent.py
@@ -6,6 +6,7 @@ Defines BaseTest class
 import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
 class BaseTest(unittest.TestCase):
     """ The BaseTest class from which tests will inherit. """
@@ -17,6 +18,9 @@ class BaseTest(unittest.TestCase):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920,1080")
         cls.driver = webdriver.Chrome(options=chrome_options)
+
+        # Global driver wait variable
+        cls.wait = WebDriverWait(cls.driver, 30)
 
     @classmethod
     def tearDownClass(cls):
