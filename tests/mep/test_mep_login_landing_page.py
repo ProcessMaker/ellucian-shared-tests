73fbabdb64592f3abff7adad4f0d9662b444398a
Kelly Hodis <kelly.hodis@processmaker.com>
2020-06-11 17:36:02 -0700

    Add waits

--- tests/mep/test_mep_login_landing_page.py
+++ tests/mep/test_mep_login_landing_page.py
@@ -3,6 +3,8 @@
 import unittest
 from test_parent import BaseTest
 from util import run_test
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestLoginPage(BaseTest):
@@ -14,6 +16,8 @@ class TestLoginPage(BaseTest):
         self.driver.get(data['server_url'])
 
         # Verify that workspace selection and submit button are displayed
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'select-workspace')))
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'display_select_input')))
         self.assertTrue(self.driver.find_element_by_class_name('select-workspace').is_displayed())
         self.assertTrue(self.driver.find_element_by_id('display_select_input').is_displayed())
 
