44de6cf77bd64b55ccdcec4249b00a926a555754
swim-mer <kelly.hodis@processmaker.com>
2020-06-23 13:51:16 -0700

    Remove self in method parameter definitions

--- includes/api_requests.py
+++ includes/api_requests.py
@@ -3,7 +3,7 @@
 import json
 import requests
 
-def parse_response(self, auth):
+def parse_response(auth):
     ''' Method to verify variable list can be parsed as JSON. '''
     try:
         json.loads(get_variable_list(auth))
@@ -11,20 +11,20 @@ def parse_response(self, auth):
     except json.decoder.JSONDecodeError:
         return False
 
-def get_variable_list(self, auth):
+def get_variable_list(auth):
     ''' Method to get variable list using project id. '''
     project_id, token = get_project_id(auth)
     response = requests.get('https://mep.dev-ellucian-us.processmaker.com/api/1.0/mep/project/' + project_id + '/process-variables', headers=token)
     return response.text
 
-def get_project_id(self, auth):
+def get_project_id(auth):
     ''' Method to get project id of first process in process list. '''
     token = {"Authorization": 'Bearer ' + get_access_token(auth)}
     response = requests.get('https://mep.dev-ellucian-us.processmaker.com/api/1.0/mep/project', headers=token)
     return (json.loads(response.text)[0]['prj_uid'], token)
 
 ''' Once requests module is installed into Docker container, move this method to util.py. '''
-def get_access_token(self, auth):
+def get_access_token(auth):
     ''' Method to grab access token through password grant. '''
     payload = [
         ("grant_type", "password"),
