d39b13f8ae4cda629e5e1b52cb0a81ad4d8bf691
Kelly Hodis <kelly.hodis@processmaker.com>
2020-06-23 13:44:32 -0700

    Move api request methods to their own file

--- tests/test_3_variables_load.py
+++ tests/test_3_variables_load.py
@@ -47,42 +47,6 @@ class TestVariablesLoad(BaseTest):
         self.assertTrue(parse_response(auth))
 
 
-    def parse_response(self, auth):
-        ''' Method to verify variable list can be parsed as JSON. '''
-        try:
-            json.loads(self.get_variable_list(auth))
-            return True
-        except json.decoder.JSONDecodeError:
-            return False
-
-    def get_variable_list(self, auth):
-        ''' Method to get variable list using project id. '''
-        project_id, token = self.get_project_id(auth)
-        response = requests.get('https://mep.dev-ellucian-us.processmaker.com/api/1.0/mep/project/' + project_id + '/process-variables', headers=token)
-        return response.text
-
-    def get_project_id(self, auth):
-        ''' Method to get project id of first process in process list. '''
-        token = {"Authorization": 'Bearer ' + self.get_access_token(auth)}
-        response = requests.get('https://mep.dev-ellucian-us.processmaker.com/api/1.0/mep/project', headers=token)
-        return (json.loads(response.text)[0]['prj_uid'], token)
-
-    ''' Once requests module is installed into Docker container, move this method to util.py. '''
-    def get_access_token(self, auth):
-        ''' Method to grab access token through password grant. '''
-        payload = [
-            ("grant_type", "password"),
-            ("scope", "*"),
-            ("client_id", auth['client_id']),
-            ("client_secret", auth['client_secret']),
-            ("username", auth['username']),
-            ("password", auth['password'])
-            ]
-        response = requests.post('https://mep.dev-ellucian-us.processmaker.com/mep/oauth2/token', data=payload)
-        text = json.loads(response.text)
-        return text['access_token']
-
-
 if __name__ == "__main__":
     import __main__
     output = run_test(TestVariablesLoad, data, __main__)
