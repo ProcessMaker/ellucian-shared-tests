204cc852bac9d459e0b31e52be64b0bb05a4826a
Kelly Hodis <kelly.hodis@processmaker.com>
2020-06-29 09:51:32 -0700

    Edit wait

--- tests/test_2_component_versions.py
+++ tests/test_2_component_versions.py
@@ -19,7 +19,7 @@ class TestComponentVersions(BaseTest):
         self.driver = login(data, self.driver)
         
         # Wait for Processes page to load
-        self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Admin')))
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'SETUP')))
         
         # Navigate to Admin / System Info page
         self.driver.find_element_by_id('SETUP').click()
