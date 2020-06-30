4e7b696498af4968a3044f06824c3f22096ac1ad
Kelly Hodis <kelly.hodis@processmaker.com>
2020-06-30 14:32:22 -0700

    Add logs

--- tests/test_1_up.py
+++ tests/test_1_up.py
@@ -1,11 +1,15 @@
 #!/usr/local/bin/python3
+from sys import path
+path.append('../')
 
 import unittest
+from io import StringIO
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from test_parent import BaseTest
-from util import run_test, login
+from includes.test_parent import BaseTest
+from includes.util import run_test, login
+from __init__ import data
 
 
 class TestLoginPage(BaseTest):
@@ -14,17 +18,23 @@ class TestLoginPage(BaseTest):
     def test_login(self):
         ''' Test that landing page loads. '''
         # Login using configured url, workspace, username, and password
-        self.driver = login(data, self.driver)
-        
+        self.log.append('Attempting login...')
+        self.driver = login(data, self.driver, self.log)
+
         # Wait for Processes page to load
+        self.driver.log.append('Waiting for main page to load')
         self.wait.until(EC.visibility_of_element_located((By.ID, 'SETUP')))
-        
+
         # Verify that Processes page elements have loaded
         self.assertTrue(self.driver.find_element_by_id('pm_main_table'))
         self.assertTrue(self.driver.find_element_by_id('pm_header'))
         self.assertTrue(self.driver.find_element_by_class_name('Footer'))
 
+        self.driver.log.append('Test passed')
+        self.log = self.driver.log
+
 
 if __name__ == "__main__":
     import __main__
     output = run_test(TestLoginPage, data, __main__)
+    print(output)
