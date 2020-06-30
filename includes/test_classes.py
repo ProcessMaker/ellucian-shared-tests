0eda3d2b2ea15f11dae69fd4c49fc66619fbc8b3
Kelly Hodis <kelly.hodis@processmaker.com>
2020-06-30 14:37:55 -0700

    Edit comments

--- includes/test_classes.py
+++ includes/test_classes.py
@@ -1,30 +1,7 @@
 #!/usr/local/bin
-
 """
-Subclass unittest.TestCase
-DONE --> add a class attribute that is a StringIO instance
---> --> to get page sources (DONE) and parse with regex (for HTML)
-DONE --> add a class attribute that is a list
---> --> to add comments, notes, debug info, etc
-
-Subclass unittest.TextTestRunner
-DONE --> add _makeResult() method to call custom TextTestResult
---> define self.attribute (custom TestCase StringIO object) inside run() method
---> define result.attribute = self.attribute (from run() method) inside _makeResult() method
---> return result (_makeResult() method)
-
-Subclass unittest.TextTestResult
---> 
-
-Subclass unittest.TestSuite
---> Add 
-
-Inside util run_test method
---> log object.read() to get logs
-
-
+Classes to extend unittest classes.
 """
-
 
 import unittest
 
