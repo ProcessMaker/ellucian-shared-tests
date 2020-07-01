54fde766b034114c35801400520ad9524ec9a762
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-01 11:54:32 -0700

    Readd test for mysql version and add logs

---
+++ tests/test_2_1_mysql_version.py
@@ -0,0 +1,59 @@
+#!/usr/local/bin/python3
+
+import unittest
+import json
+import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from test_parent import BaseTest
+from util import run_test, login, read_from_json_file
+
+
+class TestComponentVersions(BaseTest):
+    ''' Class to navigate to Admin / System Info page. '''
+
+    def test_component_version(self):
+        ''' Test that versions are correct. '''
+        # Login using configured url, workspace, username, and password
+        self.log.append('Attempting login...')
+        self.driver = login(data, self.driver)
+        
+        # Wait for Processes page to load
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'SETUP')))
+        
+        # Navigate to Admin / Case Builder Cache Info page
+        self.driver.find_element_by_id('SETUP').click()
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'adminFrame')))
+        self.driver.switch_to.frame(self.driver.find_element_by_id('adminFrame'))
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'Case List Cache Builder')]")))
+        self.driver.find_element_by_xpath("//span[contains(text(),'Case List Cache Builder')]").click()
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'setup-frame')))
+        self.driver.switch_to.frame(self.driver.find_element_by_id('setup-frame'))
+
+        # Get version values displayed on Case Builder Cache Info page
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'ext-gen12')))
+        from time import sleep
+        sleep(1)
+        cache_info = self.driver.find_element_by_id('ext-gen12').text
+        mysql = re.search(r'(?<=MySQL Version\s)([^\s]+)(?=-)', cache_info).group(0)
+        
+        # Get expected RDS versions from expected_values.json
+        expected_versions = read_from_json_file(self.driver.data['repository_path'],
+                                                '/includes/expected_values.json', 'RDS')
+        
+        # Verify versions match expected
+        # Assert MySQL version
+        try:
+            self.assertTrue(expected_versions['mysql'] in mysql)
+            self.log.append('Correct MySQL version')
+        except:
+            self.log.append('Wrong MySQL version')
+            self.fail()
+
+        self.log = self.driver.log
+
+if __name__ == "__main__":
+    import __main__
+    data['repository_path'] = repository_path
+    output = run_test(TestComponentVersions, data, __main__)
