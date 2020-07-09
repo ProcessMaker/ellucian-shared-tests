8581245b39c3f7ef6e051af20631b8ff8e01a63d
swim-mer <kelly.hodis@processmaker.com>
2020-07-08 17:18:04 -0700

    Condense wait methods

--- tests/test_5_plugin_versions.py
+++ tests/test_5_plugin_versions.py
@@ -18,16 +18,14 @@ class TestPluginVersions(BaseTest):
         self.driver = login(data, self.driver, self.log)
         
         # Wait for Processes page to load
-        self.wait.until(EC.visibility_of_element_located((By.ID, 'SETUP')))
+
         
         # Navigate to Admin / Plugins Manager page
-        self.driver.find_element_by_id('SETUP').click()
+        self.wait.until(EC.visibility_of_element_located((By.ID, 'SETUP'))).click()
         self.wait.until(EC.visibility_of_element_located((By.ID, 'adminFrame')))
         self.driver.switch_to.frame(self.driver.find_element_by_id('adminFrame'))
-        self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Plugins')))
-        self.driver.find_element_by_link_text('Plugins').click()
-        self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Plugins Manager")))
-        self.driver.find_element_by_link_text('Plugins Manager').click()
+        self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Plugins'))).click()
+        self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Plugins Manager"))).click()
         self.wait.until(EC.visibility_of_element_located((By.ID, 'setup-frame')))
         self.driver.switch_to.frame(self.driver.find_element_by_id('setup-frame'))
         
