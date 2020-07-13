f96b793dcece98358aaa64af0a1c3dcf5eeb2add
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-13 09:02:28 -0700

    Add class for login page

---
+++ includes/login_page.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3
+
+""" Class for Login Page """
+
+import selenium
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class LoginPage:
+    ''' Contain Login Page methods and selectors. '''
+
+    def __init__(self, driver, data):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 120)
+        self.data = data
+
+
+    def find_elements(self):
+        self.username_field = self.wait.until(EC.visibility_of_element_located((By.ID, 'form[USR_USERNAME]')))
+        self.password_field = self.wait.until(EC.visibility_of_element_located((By.ID, 'form[USR_PASSWORD_MASK]')))
+        self.workspace_dropdown = self.wait.until(EC.visibility_of_element_located((By.ID, 'form[USER_ENV]')))
+        self.submit_button = self.wait.until(EC.visibility_of_element_located((By.ID, 'form[BSUBMIT]')))
+    
+
+    def get_login_page(self):
+        self.driver.get(self.data['server_url'])
+
+
+    def login(self):
+        self.get_login_page()
+        self.find_elements()
+        self.driver.log.append('Attempting login...')
+        self.username_field.send_keys(self.data['username'])
+        self.password_field.send_keys(self.data['password'])
+        self.workspace_dropdown.send_keys(self.data['server_workspace'])
+        self.submit_button.click()
+        return self.driver
+
