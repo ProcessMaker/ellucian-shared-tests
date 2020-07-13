9b8fd3060f27277576a86eccb2dc87d5a593a22e
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-13 09:01:29 -0700

    Move log assignments to BaseTest

--- includes/test_parent.py
+++ includes/test_parent.py
@@ -11,21 +11,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 class BaseTest(unittest.TestCase):
     """ The BaseTest class from which tests will inherit. """
     page = ''
-    log = []
+    log = ['Log created']
 
     @classmethod
     def setUpClass(cls):
         ''' Set up browser instance. '''
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        #chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920,1080")
         cls.driver = webdriver.Chrome(options=chrome_options)
 
         # Global driver wait variable
         cls.wait = WebDriverWait(cls.driver, 120)
 
+        # Assign log to driver
+        cls.driver.log = BaseTest.log
+
     @classmethod
     def tearDownClass(cls):
         ''' End browser instance. '''
+        # Reassign log to class attribute
+        BaseTest.log = cls.driver.log
+
         cls.driver.close()
         cls.driver.quit()
