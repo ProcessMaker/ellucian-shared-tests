9b39f82cac69fdadb939559bdeb95143e4b4a1cf
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-14 08:41:03 -0700

    Add method for getting system information

--- includes/admin_page.py
+++ includes/admin_page.py
@@ -24,6 +24,11 @@ class AdminPage:
     def find_elements_on_adminFrame(self):
         self.case_list_cache_builder = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'Case List Cache Builder')]")))
         self.setup_frame = self.wait.until(EC.visibility_of_element_located((By.ID, 'setup-frame')))
+        self.system_information = self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'System information')))
+
+    def find_system_information_elements(self):
+        self.process_information_table = self.wait.until(EC.visibility_of_element_located((By.ID, 'ext-gen13-gp-section-Process Information-bd')))
+        self.system_information_table = self.wait.until(EC.visibility_of_element_located((By.ID, 'ext-gen13-gp-section-System information-bd')))
 
     def find_case_list_cache_builder_elements(self):
         self.workflow_applications_cache_info = self.wait.until(EC.visibility_of_element_located((By.ID, 'ext-gen12')))
@@ -37,13 +42,22 @@ class AdminPage:
     def switch_to_setup_frame(self):
         self.driver.switch_to.frame(self.setup_frame)
 
-    def get_case_list_cache_builder(self):
+    def get_adminFrame(self):
         self.get_admin_page()
         self.find_elements()
         self.switch_to_adminFrame()
         self.find_elements_on_adminFrame()
+
+    def get_case_list_cache_builder(self):
+        self.get_adminFrame()
         self.case_list_cache_builder.click()
         self.switch_to_setup_frame()
         self.find_case_list_cache_builder_elements()
         return self.workflow_applications_cache_info.text
         
+    def get_system_information(self):
+        self.get_adminFrame()
+        self.system_information.click()
+        self.switch_to_setup_frame()
+        self.find_system_information_elements()
+        return (self.process_information_table.text, self.system_information_table.text)
