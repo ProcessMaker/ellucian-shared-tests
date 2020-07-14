0b4c83c0b799f5dbe1d8fe5613be4ed0a3775da3
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-14 09:48:35 -0700

    Move navigation to OauthPage class

--- tests/test_3_variables_load.py
+++ tests/test_3_variables_load.py
@@ -5,9 +5,10 @@ import json
 import re
 import unittest
 from test_parent import BaseTest
-from util import run_test, login
+from util import run_test, login, regex
 from api_requests import parse_response
 from login_page import LoginPage
+from oauth_page import OauthPage
 
 
 class TestVariablesLoad(BaseTest):
@@ -19,28 +20,18 @@ class TestVariablesLoad(BaseTest):
 
         self.driver = LoginPage(self.driver, self.data).login()
 
-        # Navigate to Oauth2 Applications page to get client id and client secret
-        self.driver.get(data_['server_url'] + '/sys' + data_['server_workspace'] + '/en/neoclassic/oauth2/applications')
-        self.wait.until(EC.visibility_of_element_located((By.ID, 'adminFrame')))
-        self.driver.switch_to.frame(self.driver.find_element_by_id('adminFrame'))
+        oauth_detail = OauthPage(self.driver, self.data).get_oauth_detail()
 
-        # Inspect Ellucian workflow
-        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'x-grid3-row')))
-        self.driver.find_element_by_class_name('x-grid3-row').click()
-        self.driver.find_element_by_id('ext-gen42').click()
-        self.wait.until(EC.visibility_of_element_located((By.ID, 'ext-comp-1010')))
+        client_id = regex("r'(?<=ID:\s)\w+'", oauth_detail)
+        client_secret = regex("r'(?<=Secret:\s)\w+'", oauth_detail)
 
-        # Get client id and client secret from window
-        window_text = self.driver.find_element_by_id('ext-comp-1010').text
-        client_id = re.search(r'(?<=ID:\s)\w+', window_text).group(0)
-        client_secret = re.search(r'(?<=Secret:\s)\w+', window_text).group(0)
         self.driver.api = {
-            'url': self.driver.data['server_url'],
-            'workspace': self.driver.data['server_workspace']
+            'url': data['server_url'],
+            'workspace': data['server_workspace']
         }
         auth = {
-            'username': self.driver.data['username'],
-            'password': self.driver.data['password'],
+            'username': data['username'],
+            'password': data['password'],
             'client_id': client_id,
             'client_secret': client_secret
         }
