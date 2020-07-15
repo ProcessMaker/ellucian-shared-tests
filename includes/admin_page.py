6071a18f65140dbb347edb63e2bfef04d6dd56b6
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-15 15:50:41 -0700

    Add plugins selectors and methods

--- includes/admin_page.py
+++ includes/admin_page.py
@@ -17,13 +17,32 @@ class AdminPage:
         self.data = data
         self.page_url = self.data['server_url'] + '/sys' + data['server_workspace'] + '/en/ellucianux/setup/main'
 
-
     def find_elements(self):
         self.adminFrame = self.wait.until(EC.visibility_of_element_located((By.ID, 'adminFrame')))
 
+    def get_admin_page(self):
+        self.driver.get(self.page_url)
+
+    def switch_to_adminFrame(self):
+        self.driver.switch_to.frame(self.adminFrame)
+
+    def switch_to_setup_frame(self):
+        self.driver.switch_to.frame(self.wait.until(EC.visibility_of_element_located((By.ID, 'setup-frame'))))
+
+    def get_adminFrame(self):
+        self.get_admin_page()
+        self.find_elements()
+        self.switch_to_adminFrame()
+        self.find_elements_on_adminFrame()
+
     def find_elements_on_adminFrame(self):
+        self.plugins = self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Plugins')))
+ 
+    ''' Settings panel
+    '''
+
+    def find_elements_on_settings_tab(self):
         self.case_list_cache_builder = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//span[contains(text(),'Case List Cache Builder')]")))
-        self.setup_frame = self.wait.until(EC.visibility_of_element_located((By.ID, 'setup-frame')))
         self.system_information = self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'System information')))
 
     def find_system_information_elements(self):
@@ -32,24 +51,10 @@ class AdminPage:
 
     def find_case_list_cache_builder_elements(self):
         self.workflow_applications_cache_info = self.wait.until(EC.visibility_of_element_located((By.ID, 'ext-gen12')))
-    
-    def get_admin_page(self):
-        self.driver.get(self.page_url)
-
-    def switch_to_adminFrame(self):
-        self.driver.switch_to.frame(self.adminFrame)
-
-    def switch_to_setup_frame(self):
-        self.driver.switch_to.frame(self.setup_frame)
-
-    def get_adminFrame(self):
-        self.get_admin_page()
-        self.find_elements()
-        self.switch_to_adminFrame()
-        self.find_elements_on_adminFrame()
 
     def get_case_list_cache_builder(self):
         self.get_adminFrame()
+        self.find_elements_on_settings_tab()
         self.case_list_cache_builder.click()
         self.switch_to_setup_frame()
         self.find_case_list_cache_builder_elements()
@@ -61,3 +66,17 @@ class AdminPage:
         self.switch_to_setup_frame()
         self.find_system_information_elements()
         return (self.process_information_table.text, self.system_information_table.text)
+
+    ''' Plugins panel
+    '''
+    def find_elements_on_plugins_tab(self):
+        self.plugins_manager = self.wait.until(EC.visibility_of_element_located((By.LINK_TEXT, "Plugins Manager")))
+
+    def get_plugins(self):
+        self.get_adminFrame()
+        self.find_elements_on_adminFrame()
+        self.plugins.click()
+        self.plugins_manager.click()
+        self.switch_to_setup_frame()
+        # Double check this line
+        return [element.text for element in self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'x-grid3-row')))]
