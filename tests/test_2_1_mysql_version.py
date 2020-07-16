fd363c3cdd6e9cb64f167f4e0e5fcf06ef398c97
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-16 09:43:39 -0700

    Add check

--- tests/test_2_1_mysql_version.py
+++ tests/test_2_1_mysql_version.py
@@ -17,8 +17,9 @@ class TestRDSVersions(BaseTest):
 
         self.driver = LoginPage(self.driver, self.data).login()
         
-        mysql = regex("r'(?<=MySQL Version\s)([^\s]+)(?=-)'",
-                      AdminPage(self.driver, self.data).get_case_list_cache_builder())
+        html = AdminPage(self.driver, self.data).get_case_list_cache_builder()
+        if html:
+            mysql = regex("r'(?<=MySQL Version\s)([^\s]+)(?=-)'", html)
         
         # Get expected RDS versions from expected_values.json
         expected_versions = read_from_json_file(self.driver.data['repository_path'],
