b13315e387229d1ea5a20216e17e99569f8ddd36
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-27 15:18:10 -0700

    Add test for new project dropdown

---
+++ tests/test_7_two_elements.py
@@ -0,0 +1,43 @@
+#!/usr/local/bin/python3
+"""
+Test 7: Two Elements
+
+Prerequisites: URL, Username, Password
+Testing: Functionality
+
+Purpose
+
+The purpose of this test is to verify there are two elements in dropdown menu after clicking “New” on Designer page.
+
+Test Steps
+Visit the URL
+Login
+Visit the Designer page
+Click “New”
+Verify there are two elements in dropdown menu
+"""
+
+import unittest
+import json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from test_parent import BaseTest
+from util import run_test
+from login_page import LoginPage
+from designer_page import DesignerPage
+
+
+class TestDesignerNewDropDown(BaseTest):
+    ''' Class to test Designer's New dropdown menu. '''
+
+    def test_designer_new_project_dropdown(self):
+        ''' Verify two elements in dropdown. '''
+        self.driver = LoginPage(self.driver, data).login()
+
+        self.assertTrue(DesignerPage(self.driver, data).new_project_has_two_elements())
+
+
+if __name__ == "__main__":
+    import __main__
+    output = run_test(TestDesignerNewDropDown, data, __main__)
