34a014d27519496330bd644850cdfe9a5ccc2668
Kelly Hodis <kelly.hodis@processmaker.com>
2020-07-27 11:47:04 -0700

    Edit variable name

--- tests/test_6_enterprise_plugin_versions.py
+++ tests/test_6_enterprise_plugin_versions.py
@@ -26,32 +26,32 @@ class TestEnterprisePluginVersions(BaseTest):
         self.driver = LoginPage(self.driver, self.data).login()
 
         # Retrieve Custom Plugins dictionary from expected_values.json
-        custom_plugins = read_from_json_file(self.data['repository_path'],
+        enterprise_plugins = read_from_json_file(self.data['repository_path'],
                                              '/includes/expected_values.json', 'Enterprise Plugins')
 
         # Verify all custom plugins are visible on page with correct version, and are enabled
-        enterprise_plugins = AdminPage(self.driver, self.data).get_enterprise_plugins()
+        plugins = AdminPage(self.driver, self.data).get_enterprise_plugins()
 
         self.driver.log.append('Enterprise Plugin versions: ')
         for elem in plugins:
-            for key in custom_plugins.keys():
+            for key in enterprise_plugins.keys():
                 if key in elem:
                     try:
-                        self.assertTrue(custom_plugins[key] in elem)
+                        self.assertTrue(enterprise_plugins[key] in elem)
                         self.driver.log[-1] += 'Correct ' + key + ', '
                     except AssertionError as e:
                         self.driver.log[-1] += 'Wrong ' + key + ' version, '
                         self.assertionFailures.append(str(e))
 
-                    del custom_plugins[key]
+                    del enterprise_plugins[key]
                     break 
         
-        # Assert custom_plugins is empty (meaning every expected value was found)
+        # Assert enterprise_plugins is empty (meaning every expected value was found)
         try:
-            self.assertEqual(custom_plugins, {})
+            self.assertEqual(enterprise_plugins, {})
             self.driver.log[-1] += 'All found'
         except AssertionError as e:
-            not_found = ', '.join(custom_plugins)
+            not_found = ', '.join(enterprise_plugins)
             self.driver.log[-1] += 'Not found: ' + not_found
             self.assertionFailures.append(str(e))
 
