4bcfbc257f4cba4519e6cd1aa8b00047de3ed836
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-10 10:20:21 -0700

    Remove page attribute

--- includes/test_classes.py
+++ includes/test_classes.py
@@ -7,13 +7,11 @@ import unittest
 
 class CustomTestSuite(unittest.TestSuite):
     ''' Subclass TestSuite to add custom attributes. '''
-    page = ''
     log = []
 
-    def run(self, result): 
+    def run(self, result):
         ''' Add test case attributes to result. '''
         for test in self._tests:
-            result.page = test.page
             result.log = test.log
         return super(CustomTestSuite, self).run(result)
 
@@ -24,11 +22,10 @@ class CustomTestLoader(unittest.TestLoader):
 class CustomTextTestResult(unittest.TextTestResult):
     ''' Subclass TextTestResult to add custom results. '''
 
-    def __init__(self, stream, descriptions, verbosity, log, page):
+    def __init__(self, stream, descriptions, verbosity, log):
         """Initializes the test number generator, then calls super impl"""
         super(CustomTextTestResult, self).__init__(stream, descriptions, verbosity)
         self.log = log
-        self.page = page
 
 
 class CustomTextTestRunner(unittest.TextTestRunner):
@@ -39,11 +36,9 @@ class CustomTextTestRunner(unittest.TextTestRunner):
     def run(self, test_suite):
         ''' Return the custom TextTestResult object. '''
 
-        self.page = test_suite.page
         self.log = test_suite.log
         return super(CustomTextTestRunner, self).run(test_suite)
 
     def _makeResult(self):
         """Creates and returns a result instance that has custom attributes"""
-        return CustomTextTestResult(self.stream, self.descriptions, self.verbosity, self.log, self.page)
-
+        return CustomTextTestResult(self.stream, self.descriptions, self.verbosity, self.log)
