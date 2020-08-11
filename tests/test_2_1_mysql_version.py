58672b3d3cbb4fac03321cd2014f04b32f0fd12d
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-11 16:38:46 -0700

    Edit html tag

--- tests/test_2_1_mysql_version.py
+++ tests/test_2_1_mysql_version.py
@@ -32,7 +32,7 @@ class TestRDSVersions(BaseTest):
         # Assert MySQL version
         try:
             self.assertTrue(expected_versions['mysql'] in mysql)
-            self.log.append('Correct MySQL version: ' + '<html><br />' + expected_versions['mysql'] + '</html>')
+            self.log.append('Correct MySQL version: ' + '<br />' + expected_versions['mysql'])
         except:
             self.log.append('Incorrect MySQL version: ' + mysql + ', Expected: ' + expected_versions['mysql'])
             self.fail()
