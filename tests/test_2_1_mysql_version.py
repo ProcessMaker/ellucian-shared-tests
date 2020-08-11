4f17f17ea76c4b68a666cf84799fcf26fb750b29
Kelly Hodis <kelly.hodis@processmaker.com>
2020-08-11 13:40:00 -0700

    Add diff

--- tests/test_2_1_mysql_version.py
+++ tests/test_2_1_mysql_version.py
@@ -16,7 +16,7 @@ class TestRDSVersions(BaseTest):
         ''' Test that MySQL version is correct. '''
 
         self.driver = LoginPage(self.driver, self.data).login()
-        
+
         html = AdminPage(self.driver, self.data).get_case_list_cache_builder()
         if html:
             mysql = re.search(r'(?<=MySQL Version\s)([^\s]+)(?=-)', html).group(0)
@@ -24,14 +24,14 @@ class TestRDSVersions(BaseTest):
         # Get expected RDS versions from expected_values.json
         expected_versions = read_from_json_file(self.data['repository_path'],
                                                 '/includes/expected_values.json', 'RDS')
-        
+
         # Verify versions match expected
         # Assert MySQL version
         try:
             self.assertTrue(expected_versions['mysql'] in mysql)
-            self.log.append('Correct MySQL version')
+            self.log.append('Correct MySQL version: ' + expected_versions['mysql'])
         except:
-            self.log.append('Wrong MySQL version')
+            self.log.append('Actual MySQL version: ' + mysql + ', Expected: ' + expected_versions['mysql'])
             self.fail()
 
 
